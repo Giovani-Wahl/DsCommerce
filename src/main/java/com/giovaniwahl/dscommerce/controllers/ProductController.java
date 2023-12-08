2b20b683956699dfbb551694ab09aa011e25fb02
wahl <giovaniwahl@gmail.com>
2023-12-08 08:44:52 -0300

    teste do repository

--- src/main/java/com/giovaniwahl/dscommerce/controllers/ProductController.java
+++ src/main/java/com/giovaniwahl/dscommerce/controllers/ProductController.java
@@ -1,15 +1,27 @@
 package com.giovaniwahl.dscommerce.controllers;
 
+import com.giovaniwahl.dscommerce.domain.entities.Product;
+import com.giovaniwahl.dscommerce.domain.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private final ProductRepository productRepository;
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @GetMapping
     public String teste(){
-        return "Olá Mundo";
+        Optional<Product> result = productRepository.findById(1L);
+        Product product = result.get();
+        return product.getName();
     }
 }
