20e8b78acff6cebf8f9d64ddb73c616e351f9418
wahl <giovaniwahl@gmail.com>
2024-02-14 10:44:16 -0300

    userDatails and GrantedAuthority

--- src/main/java/com/giovaniwahl/dscommerce/domain/entities/Role.java
+++ src/main/java/com/giovaniwahl/dscommerce/domain/entities/Role.java
@@ -1,6 +1,7 @@
 package com.giovaniwahl.dscommerce.domain.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -8,21 +9,14 @@ import java.util.Set;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
-    @ManyToMany
-    @JoinTable(name = "tb_user_role",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
-
     public Role() {
     }
-
     public Role(Long id, String authority) {
         this.id = id;
         this.authority = authority;
@@ -35,27 +29,12 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
     public void setAuthority(String authority) {
         this.authority = authority;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void addRole(Role role){
-        roles.add(role);
-    }
-    public boolean hasRole(String roleName){
-        for (Role role : roles){
-            if (role.getAuthority().equals(roleName)){
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
--- src/main/java/com/giovaniwahl/dscommerce/domain/entities/User.java
+++ src/main/java/com/giovaniwahl/dscommerce/domain/entities/User.java
@@ -1,17 +1,17 @@
 package com.giovaniwahl.dscommerce.domain.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class  User implements Serializable {
+public class  User implements UserDetails {
     @Serial
     private static final long serialVersionUID= 1L;
     @Id
@@ -26,6 +26,12 @@ public class  User implements Serializable {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User(){
     }
@@ -83,6 +89,46 @@ public class  User implements Serializable {
 
     public List<Order> getOrders() {
         return orders;
+    }
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void addRole(Role role){
+        roles.add(role);
+    }
+    public boolean hasRole(String roleName){
+        for (Role role : roles){
+            if (role.getAuthority().equals(roleName)){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
