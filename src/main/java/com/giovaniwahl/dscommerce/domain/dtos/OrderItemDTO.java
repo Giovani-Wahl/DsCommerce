da2b2e8212f0110d2e9c0017f6e1c5e6f96bab6d
wahl <giovaniwahl@gmail.com>
2024-03-05 09:05:05 -0300

    inclusão imagem na culsulta pedido

--- src/main/java/com/giovaniwahl/dscommerce/domain/dtos/OrderItemDTO.java
+++ src/main/java/com/giovaniwahl/dscommerce/domain/dtos/OrderItemDTO.java
@@ -7,18 +7,21 @@ public class OrderItemDTO {
      private String name;
      private Double price;
      private Integer quantity;
+     private String imgUrl;
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
     public OrderItemDTO(OrderItem entity) {
         productId = entity.getProduct().getId();
         name = entity.getProduct().getName();
         price = entity.getPrice();
         quantity = entity.getQuantity();
+        imgUrl = entity.getProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -50,5 +53,12 @@ public class OrderItemDTO {
     }
     public Double getSubTotal(){
         return price * quantity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 }
