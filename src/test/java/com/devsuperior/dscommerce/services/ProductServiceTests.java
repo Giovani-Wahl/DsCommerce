81e0e819c462076b502e94e7b210178cbc882df2
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-02 10:15:36 -0300

    unit test ProductService findAll, ok.

--- src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -12,9 +13,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -26,6 +34,7 @@ public class ProductServiceTests {
     private Product product;
     private Long existingId,nonExistingId;
     private String productName;
+    private PageImpl<Product> page;
 
     @BeforeEach
     void setUp() throws Exception{
@@ -33,9 +42,12 @@ public class ProductServiceTests {
         nonExistingId = 2L;
         productName = "PlayStation 5";
         product = ProductFactory.createProduct(productName);
+        page = new PageImpl<>(List.of(product));
 
         Mockito.when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
         Mockito.when(productRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        Mockito.when(productRepository.searchByName(any(),(Pageable)any())).thenReturn(page);
     }
 
     @Test
@@ -52,5 +64,14 @@ public class ProductServiceTests {
         Assertions.assertThrows(ResourceNotFoundException.class,()->{
             productService.findById(nonExistingId);
         });
+    }
+    @Test
+    public void findAllShouldReturnPagedProductMinDto(){
+        Pageable pageable = PageRequest.of(0,12);
+        String name = "PlayStation 5";
+        Page<ProductMinDTO> result = productService.findAll(name,pageable);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getSize(),1);
+        Assertions.assertEquals(result.iterator().next().getName(),productName);
     }
 }
