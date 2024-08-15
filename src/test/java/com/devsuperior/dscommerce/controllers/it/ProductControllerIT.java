dc6facdfb14cd33b17aa5893dfb16baea247780d
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-15 12:35:20 -0300

    integration test insertShouldReturnProductDtoWhenAdminLogged

--- src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -1,10 +1,16 @@
 package com.devsuperior.dscommerce.controllers.it;
 
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.tests.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -22,10 +29,36 @@ public class ProductControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private TokenUtil tokenUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String clientUserName,clientPassword,adminUserName,adminPassword;
+    private String adminToken,clientToken,invalidToken;
     private String productName;
+    private Product product;
+    private ProductDTO productDTO;
 
     @BeforeEach
-    void setUp(){
+    void setUp()throws Exception{
+        clientUserName = "maria@gmail.com";
+        clientPassword = "123456";
+        adminUserName = "alex@gmail.com";
+        adminPassword = "123456";
+
+        adminToken = tokenUtil.obtainAccessToken(mockMvc,adminUserName,adminPassword);
+        clientToken = tokenUtil.obtainAccessToken(mockMvc,clientUserName,clientPassword);
+        invalidToken = adminToken +"invalid";
+
+        Category category = new Category(2L,"Categoria Teste");
+        product = new Product(null,"PS5 Pro","descrição do item cadastrado",3999.90,
+                "uri da imagem do produto");
+        product.getCategories().add(category);
+
+        productDTO =new ProductDTO(product);
+
         productName = "Macbook";
     }
 
@@ -53,5 +86,24 @@ public class ProductControllerIT {
         result.andExpect(jsonPath("$.content[0].name").isNotEmpty());
         result.andExpect(jsonPath("$.content[0].price").isNotEmpty());
         result.andExpect(jsonPath("$.content[0].imgUrl").isNotEmpty());
+    }
+    @Test
+    public void insertShouldReturnProductDtoWhenAdminLogged() throws Exception{
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =
+                mockMvc.perform(post("/products")
+                        .header("Authorization", "Bearer " + adminToken)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andDo(MockMvcResultHandlers.print());
+        result.andExpect(status().isCreated());
+        result.andExpect(jsonPath("$.id").isNotEmpty());
+        result.andExpect(jsonPath("$.name").value("PS5 Pro"));
+        result.andExpect(jsonPath("$.description").value("descrição do item cadastrado"));
+        result.andExpect(jsonPath("$.price").value(3999.90));
+        result.andExpect(jsonPath("$.imgUrl").value("uri da imagem do produto"));
+        result.andExpect(jsonPath("$.categories").isNotEmpty());
+        result.andExpect(jsonPath("$.categories[0].id").value(2));
     }
 }
