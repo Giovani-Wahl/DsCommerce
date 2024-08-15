bfbce8893c29bd8ef86006938b34d13ce7efe2ef
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-15 10:33:16 -0300

    integration test findAllShouldReturnPageWhenNameParamIsNotEmpty

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dscommerce.controllers.it;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private String productName;
+
+    @BeforeEach
+    void setUp(){
+        productName = "Macbook";
+    }
+
+    @Test
+    public void findAllShouldReturnPageWhenNameParamIsNotEmpty() throws Exception{
+        ResultActions result =
+                mockMvc.perform(get("/products?name={productName}",productName)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.content[0].id").isNotEmpty());
+        result.andExpect(jsonPath("$.content[0].id").value(3L));
+        result.andExpect(jsonPath("$.content[0].name").isNotEmpty());
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[0].price").isNotEmpty());
+        result.andExpect(jsonPath("$.content[0].imgUrl").isNotEmpty());
+    }
+}
