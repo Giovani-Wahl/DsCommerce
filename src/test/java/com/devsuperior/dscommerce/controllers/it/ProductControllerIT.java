2a8b59fbf83a810eadac54f64bbba8b0c84dcbde
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-15 15:00:06 -0300

    integration test  insertShouldReturnUnprocessableEntityWhenAdminLoggedAndInvalidDescription

--- src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -120,4 +120,18 @@ public class ProductControllerIT {
                                 .accept(MediaType.APPLICATION_JSON));
         result.andExpect(status().isUnprocessableEntity());
     }
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndInvalidDescription() throws Exception{
+        product.setDescription("ab");
+        productDTO = new ProductDTO(product);
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result =
+                mockMvc.perform(post("/products")
+                        .header("Authorization", "Bearer " + adminToken)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isUnprocessableEntity());
+    }
 }
